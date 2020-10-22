caee95533fb972b50f57835dbba63e5705adb128
ngankhanh98 <ngankhanh98@gmail.com>
2020-10-22 17:14:29 +0700

    user very basic crud

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
 
   const options = new DocumentBuilder()
     .setTitle('TODO')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,33 +1,28 @@
-import {
-  Injectable,
-  InternalServerErrorException
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { plainToClass } from 'class-transformer';
-import { Repository } from 'typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { User } from '../entities/user.entity';
-import { getUserDTO } from './dto/user.dto';
 
 @Injectable()
-export class UserService {
-  constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
-
-  public async getDetail(username): Promise<getUserDTO> {
-    try {
-      const user = await this.find(username);
-      return plainToClass(getUserDTO, user[0]);
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
+export class UserService extends TypeOrmCrudService<User> {
+  constructor(@InjectRepository(User) userRepository) {
+    super(userRepository);
   }
 
-  private async find(username: string) {
-    try {
-      return await this.userRepository.find({ username: username });
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
-  }
+  // public async getDetail(username): Promise<getUserDTO> {
+  //   try {
+  //     const user = await this.find(username);
+  //     return plainToClass(getUserDTO, user[0]);
+  //   } catch (error) {
+  //     throw new InternalServerErrorException(error.message);
+  //   }
+  // }
+
+  // private async find(username: string) {
+  //   try {
+  //     return await this.userRepository.find({ username: username });
+  //   } catch (error) {
+  //     throw new InternalServerErrorException(error.message);
+  //   }
+  // }
 }
