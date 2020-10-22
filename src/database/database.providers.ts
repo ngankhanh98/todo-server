91c00d8b34278b82ae37d6579a184cba41b90c6e
ngankhanh98 <ngankhanh98@gmail.com>
2020-10-22 13:54:34 +0700

    db host remane

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 export const databaseProvider = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: 'www.db4free.net',
+  host: 'db4free.net',
   port: 3306,
   username: 'ebanking',
   password: '123456789',
