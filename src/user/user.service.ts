deba8feafd12e58c1d0a2e4fc89b5d2a584b0e24
ngankhanh98 <ngankhanh98@gmail.com>
2020-11-04 10:36:15 +0700

    schedule registry

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,18 +2,23 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { User } from '../entities/user.entity';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<User> {
-  constructor(@InjectRepository(User) private readonly userRepository) {
+  constructor(
+    @InjectRepository(User) private readonly userRepository,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {
     super(userRepository);
   }
   private readonly logger = new Logger(UserService.name);
 
-  @Cron('45 * * * * *')
+  @Cron(CronExpression.EVERY_MINUTE, { name: 'notification' })
   public async findUserByUsername(username: string) {
-    this.logger.log(this.findUserByUsername.name);
+    const job = this.schedulerRegistry.getCronJob('notification');
+    job.stop();
+    console.log(job.lastDate());
     return this.userRepository.findOne({ username: username });
   }
 }
