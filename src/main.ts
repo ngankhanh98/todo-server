e557a04fca12ac23848a875dd6eb86b5bc0b1c30
ngankhanh98 <ngankhanh98@gmail.com>
2020-11-11 09:31:33 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,29 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 
-// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // CORS setup
+  const whitelist = ['']; // TODO: create your new whitelist
+  app.enableCors({
+    origin: function(origin, callback) {
+      if (whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin blocked by CORS Policy'));
+      }
+    },
+  });
+
   app.use(compression());
   app.use(helmet());
 
-  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const options = new DocumentBuilder()
     .setTitle('TODO')
